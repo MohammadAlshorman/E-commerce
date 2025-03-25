fb520e7c22d22037b853570e88a7b40477311e8b
MohammadAlshorman <mhmdshhadhalshrman95@gmail.com>
2025-03-25 16:29:28 +0300

    Add project files.

---
+++ candy_sweets.client/src/app/Mohammad/shoe-page/shoe-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shoe-page',
+  standalone: false,
+  templateUrl: './shoe-page.component.html',
+  styleUrl: './shoe-page.component.css'
+})
+export class ShoePageComponent {
+
+}
---
+++ candy_sweets.client/src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LandingPageComponent } from './Omar/landing-page/landing-page.component';
+import { ShoePageComponent } from './Mohammad/shoe-page/shoe-page.component';
+import { ProductDetailsComponent } from './Suleiman/product-details/product-details.component';
+import { CartPageComponent } from './Sofyan/cart-page/cart-page.component';
+import { AdminDashboardComponent } from './Suhaib/admin-dashboard/admin-dashboard.component';
+import { ManageProductsComponent } from './Ahmad/manage-products/manage-products.component';
+import { ManageCategoriesComponent } from './Abdallah/manage-categories/manage-categories.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LandingPageComponent,
+    ShoePageComponent,
+    ProductDetailsComponent,
+    CartPageComponent,
+    AdminDashboardComponent,
+    ManageProductsComponent,
+    ManageCategoriesComponent
+  ],
+  imports: [
+    BrowserModule, HttpClientModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
